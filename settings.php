e0f8b70860e0adf35c3654146f78473e221d0764
Andrew Zito <zitoa@lafayette.edu>
2018-07-05 09:41:23 -0400

    Added license information to settings.php

--- settings.php
+++ settings.php
@@ -14,6 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * Settings
+ *
+ * @package    local_temporary_enrolments
+ * @copyright  2018 onwards Lafayette College ITS
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot. '/local/temporary_enrolments/lib.php');
 
